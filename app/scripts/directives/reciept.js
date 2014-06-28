a64626489f3f91cade5bda9cb71785f09a3db13e
Martin Riedel <martin.riedel@innoactive.de>
2014-06-28 15:41:21 +0200

    persistent

--- app/scripts/directives/reciept.js
+++ app/scripts/directives/reciept.js
@@ -7,7 +7,7 @@
  * # reciept
  */
 angular.module('foodNinjaApp')
-  .directive('reciept', function () {
+  .directive('reciept', function (Reciepts) {
     return {
       templateUrl: 'views/partials/reciept.html',
       restrict: 'E',
@@ -16,6 +16,10 @@ angular.module('foodNinjaApp')
       	recieptId: '='
       },
       link: function postLink(scope, element, attrs) {
+      	scope.Reciepts = Reciepts;
+      	scope.updateReciept = function() {
+      		Reciepts.updateReciept(scope.recieptId, scope.reciept);
+      	};
       	scope.initExpiration = function (product) {
       		if (!product.expiration) {
       			product.expiration = Date.now();
